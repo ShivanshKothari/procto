85cf9df7c2b3085841aeb4f116b53f32b4eff32e
shivanshkothari2004@gmail.com <shivanshkothari2004@gmail.com>
2024-11-17 19:19:22 +0530

    fixed theme provider error

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,8 +2,17 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+interface ThemeProps {
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.PropsWithChildren<ThemeProps>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
